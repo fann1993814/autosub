69e1cc854c67904009550647c4636aed8332e0ea
Jason Fan <fann1993814@gmail.com>
2022-09-11 13:41:55 +0800

    update requests version

--- setup.py
+++ setup.py
@@ -34,7 +34,7 @@ setup(
     },
     install_requires=[
         'google-api-python-client>=1.4.2',
-        'requests>=2.3.0',
+        'requests>=2.28.0',
         'pysrt>=1.0.1',
         'progressbar2>=3.34.3',
         'six>=1.11.0',
